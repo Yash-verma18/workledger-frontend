c88a3c9c1dff525d593c9a931eb13e2527c6c6ff
Yash Verma <recognizeyourself909@gmail.com>
2025-05-31 21:42:00 +0530

    Feat: Route to dashboard on wallet connect
    This commit introduces a route to the dashboard page upon successful wallet connection.
    
    - Added a new dashboard page at `src/app/dashboard/page.tsx` with basic content and metadata.
    - Updated `src/app/page.tsx` to redirect to `/dashboard` when a wallet is connected using `wagmi`'s `useAccount` hook and `next/navigation`'s `useRouter`.
    - Updated `.gitignore` to exclude `.vscode` directory.

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+export const metadata = {
+  title: 'Dashboard | MyApp',
+  description: 'Your personal dashboard page',
+};
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>Our Dashboard</h1>
+    </div>
+  );
+};
+
+export default Dashboard;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,18 @@
+'use client';
+
+import { useAccount } from 'wagmi';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 import WalletConnect from '@/components/rainbowKit/WalletConnect';
 
 export default function Home() {
+  const { isConnected } = useAccount();
+  const router = useRouter();
+  useEffect(() => {
+    if (isConnected) {
+      router.push('/dashboard');
+    }
+  }, [isConnected, router]);
   return (
     <div className='min-h-screen flex items-center justify-center bg-gray-100'>
       <WalletConnect />
