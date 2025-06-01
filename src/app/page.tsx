ff52064ec3f95198314639890a6916c39404fa14
Yash Verma <recognizeyourself909@gmail.com>
2025-06-01 16:46:54 +0530

    Feat: Enhance homepage with visual elements
    This commit enhances the homepage with visual elements to improve the user experience.
    
    - Added `next/image` to optimize image loading.
    - Integrated two SVG images (`work.svg` and `ledger.svg`) to provide visual context and appeal.
    - Modified `src/app/page.tsx` to include the images and the WalletConnect component within a flex container for better layout.
    - Added `work.svg` and `ledger.svg` to the `public` directory.
    - Removed default styles from `src/app/globals.css` to allow for more customized styling.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { useAccount } from 'wagmi';
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import WalletConnect from '@/components/rainbowKit/WalletConnect';
-
+import Image from 'next/image';
 export default function Home() {
   const { isConnected } = useAccount();
   const router = useRouter();
@@ -14,8 +14,25 @@ export default function Home() {
     }
   }, [isConnected, router]);
   return (
-    <div className='min-h-screen flex items-center justify-center '>
+    <div className='flex flex-col items-center justify-center w-full gap-8'>
+      <Image
+        src='/work.svg'
+        alt='Work'
+        width={1920}
+        height={300}
+        className='w-full max-w-[80%] object-contain mx-auto '
+        priority
+      />
+
       <WalletConnect />
+
+      <Image
+        src='/ledger.svg'
+        alt='Ledger'
+        width={1920}
+        height={300}
+        className='w-full max-w-[80%] object-contain '
+      />
     </div>
   );
 }
